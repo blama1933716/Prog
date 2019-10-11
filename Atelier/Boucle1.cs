5a3c72358e46b92223c37f54b65245690b7e09c4
blama1933716 <54861322+blama1933716@users.noreply.github.com>
2019-10-11 09:42:23 -0400

    Add files via upload

---
+++ Atelier/Boucle1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1Tableau
+{
+    class Program
+    {
+        static void AfficherIntro (ref int nb1)
+        {
+            Console.WriteLine("Veuillez saisir un nombre entier");
+            nb1 = Convert.ToInt32(Console.ReadLine());  
+        }
+        static void AfficherResultat (int tot)
+        {
+            Console.WriteLine("Le resultat est " + tot);
+        }
+        static void Main(string[] args)
+        {
+            int nb1 = 0;
+            int tot = 1; 
+
+            AfficherIntro(ref nb1);
+
+            for (int i = 1; i <= nb1; i++)
+            {
+                tot = tot * i;     
+            }
+
+            AfficherResultat(tot);
+            Console.ReadKey();
+        }
+    }
+}
